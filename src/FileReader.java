6f95dcab9f7391d16ec150dc817b0952034715eb
Angel Luis Santana Reyes <angellsant@hotmail.com>
2023-10-06 20:45:56 +0100

    primer commit

--- src/FileReader.java
+++ src/FileReader.java
@@ -6,6 +6,8 @@ public class FileReader {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+        System.out.println("masl escrito esta");
+
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
